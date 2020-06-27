6b32a3d94a0248f5a4fd9923a7d40578a19bfde2
Brian Parker <brian.parker@hotmail.com.au>
2020-06-28 02:40:43 +1000

    Initial draft

---
+++ CleanView/CleanView/Components/ICleanViewOptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanView.Components
+{
+    interface ICleanViewOptions
+    {
+        bool Transparent { get; set; }
+        string BackgroundUrl { get; set; }
+    }
+
+    class CleanViewOptions : ICleanViewOptions
+    {
+        public bool Transparent { get; set; } = true;
+        public string BackgroundUrl { get; set; } = "_content/CleanView/background.jpg";
+
+    }
+}
